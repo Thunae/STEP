02c5db459a57895f2df12856eaf2eb5ee999fffb
ryan <ryangudal@gmail.com>
2020-05-28 17:29:34 -0400

    Massive redesign of website

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,6 +26,40 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
 function successfulSubmit() {
     alert("Thank you for contacting me");
+}
+
+var intervalID = 0;
+var opacity = 0;
+
+function fadeOut(){
+   intervalID = setInterval(hide, 10);
+}
+
+function fadeIn(){
+   intervalID = setInterval(show, 10);
+}
+
+function hide(){
+    var current_item = document.getElementById("showcase");
+    opacity = Number(window.getComputedStyle(current_item).getPropertyValue("opacity"));
+    if(opacity > 0){
+        opacity = opacity - 0.05;
+        current_item.style.opacity = opacity;
+    }else{
+        clearInterval(intervalID);
+    }
+}
+
+function show(){
+    var current_item = document.getElementById("showcase");
+    opacity = Number(window.getComputedStyle(current_item).getPropertyValue("opacity"));
+    if(opacity < 1){
+        opacity = opacity + 0.05;
+        current_item.style.opacity = opacity;
+    }else{
+        clearInterval(intervalID);
+    }
 }
