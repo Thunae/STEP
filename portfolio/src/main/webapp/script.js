b36d7e18bf6e45951c4ca038c5373c3ba93232f7
ryan <ryangudal@gmail.com>
2020-05-29 14:34:13 -0400

    Transitions between pages and Reponsive mainpage

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,35 +31,8 @@ function successfulSubmit() {
     alert("Thank you for contacting me");
 }
 
-var intervalID = 0;
-var opacity = 0;
-
 function fadeOut(){
-   intervalID = setInterval(hide, 10);
-}
-
-function fadeIn(){
-   intervalID = setInterval(show, 10);
-}
-
-function hide(){
-    var current_item = document.getElementById("showcase");
-    opacity = Number(window.getComputedStyle(current_item).getPropertyValue("opacity"));
-    if(opacity > 0){
-        opacity = opacity - 0.05;
-        current_item.style.opacity = opacity;
-    }else{
-        clearInterval(intervalID);
-    }
-}
-
-function show(){
-    var current_item = document.getElementById("showcase");
-    opacity = Number(window.getComputedStyle(current_item).getPropertyValue("opacity"));
-    if(opacity < 1){
-        opacity = opacity + 0.05;
-        current_item.style.opacity = opacity;
-    }else{
-        clearInterval(intervalID);
-    }
+    const page = document.getElementById("page");
+    page.style.opacity = '0';
+    setTimeout(function(){window.location.href = "./main.html";}, 1000);
 }
