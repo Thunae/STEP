2f0a3679a6328f9dad064f8b30526e54fb03203e
ryan <ryangudal@gmail.com>
2020-06-29 20:15:35 +0000

    Replace Map values to be Strings instead of ArrayList<String>

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -25,12 +25,13 @@ import com.google.sps.Pair;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
       ArrayList<String> requiredPeople = new ArrayList<String>(request.getAttendees());
       ArrayList<String> optionalPeople = new ArrayList<String>(request.getOptionalAttendees());
-      Map<ArrayList<String>, SchedulingResult> history = new HashMap<>();
+      Map<String, SchedulingResult> history = new HashMap<>();
 
       Collection<TimeRange> answer = maximizeOptionalAttendees(requiredPeople, optionalPeople, optionalPeople.size()-1, events, request.getDuration(), history).schedule;
       return answer;
@@ -47,23 +48,23 @@ public final class FindMeetingQuery {
   }
 
   private Collection<TimeRange> createAvailableRange(ArrayList<TimeRange> bookedTimes, long duration) {
-      Collection<TimeRange> availableTimes = new ArrayList<>();
-      Collections.sort(bookedTimes, TimeRange.ORDER_BY_START);
-      int nextSlot = TimeRange.START_OF_DAY;
-      for(TimeRange filled : bookedTimes) {
-          if(filled.start() - nextSlot >= duration) {
-              availableTimes.add(TimeRange.fromStartEnd(nextSlot, filled.start(), false));
-          }
+    Collection<TimeRange> availableTimes = new ArrayList<>();
+    Collections.sort(bookedTimes, TimeRange.ORDER_BY_START);
+    int nextSlot = TimeRange.START_OF_DAY;
+    for(TimeRange filled : bookedTimes) {
+        if(filled.start() - nextSlot >= duration) {
+            availableTimes.add(TimeRange.fromStartEnd(nextSlot, filled.start(), false));
+        }
 
-          if(filled.end() > nextSlot) {
+        if(filled.end() > nextSlot) {
             nextSlot = filled.end();
-          }
-      }
+        }
+    }
 
-      if(TimeRange.END_OF_DAY - nextSlot >= duration) {
-          availableTimes.add(TimeRange.fromStartEnd(nextSlot, TimeRange.END_OF_DAY, true));
-      }
-      return availableTimes;
+    if(TimeRange.END_OF_DAY - nextSlot >= duration) {
+        availableTimes.add(TimeRange.fromStartEnd(nextSlot, TimeRange.END_OF_DAY, true));
+    }
+    return availableTimes;
   }
 
   private SchedulingResult numberOfAttendees(ArrayList<String> people, Collection<Event> events, long duration){
@@ -73,35 +74,41 @@ public final class FindMeetingQuery {
       return availableRange.isEmpty() ? new SchedulingResult(0, new ArrayList<TimeRange>()) : result;
   }
 
-  private SchedulingResult maximizeOptionalAttendees(ArrayList<String> requiredPeople, ArrayList<String> optionalPeople, int n, Collection<Event> events, long duration, Map<ArrayList<String>, SchedulingResult> history){
-    if(n<0){
-        history.put(requiredPeople, numberOfAttendees(requiredPeople, events, duration));
-        return history.get(requiredPeople);
+  private SchedulingResult maximizeOptionalAttendees(ArrayList<String> requiredPeople, ArrayList<String> optionalPeople, int n, Collection<Event> events, long duration, Map<String, SchedulingResult> history){
+    String mapID = stringify(requiredPeople);
+    if(n < 0){
+        history.put(mapID, numberOfAttendees(requiredPeople, events, duration));
+        return history.get(mapID);
     }
 
-    if(history.containsKey(requiredPeople)){
-        return history.get(requiredPeople);
+    if(history.containsKey(mapID)){
+        return history.get(mapID);
     }
-    else{
+    else {
         ArrayList<String> temp2 = new ArrayList<String>(requiredPeople);
         temp2.add(optionalPeople.get(n));
-        history.put(requiredPeople, maximizeOptionalAttendees(requiredPeople, optionalPeople, n-1, events, duration, history));
-        history.put(temp2, maximizeOptionalAttendees(temp2, optionalPeople, n-1, events, duration, history));
-        return comparison(history.get(requiredPeople), history.get(temp2)) ? history.get(requiredPeople) : history.get(temp2); 
+        String addedMapID = stringify(temp2);
+        history.put(mapID, maximizeOptionalAttendees(requiredPeople, optionalPeople, n-1, events, duration, history));
+        history.put(addedMapID, maximizeOptionalAttendees(temp2, optionalPeople, n-1, events, duration, history));
+        return comparison(history.get(mapID), history.get(addedMapID)) ? history.get(mapID) : history.get(addedMapID); 
     }
   }
 
   private class SchedulingResult{
-      public Integer numOfPeople;
-      public Collection<TimeRange> schedule;
+    public Integer numOfPeople;
+    public Collection<TimeRange> schedule;
 
-      public SchedulingResult(int num, Collection<TimeRange> schedule){
-          this.numOfPeople = num;
-          this.schedule = schedule;
-      }
+    public SchedulingResult(int num, Collection<TimeRange> schedule){
+        this.numOfPeople = num;
+        this.schedule = schedule;
+    }
   }
 
   public Boolean comparison(SchedulingResult left, SchedulingResult right){
       return left.numOfPeople > right.numOfPeople;
+  }
+
+  public String stringify(ArrayList<String> array){
+      return array.stream().collect(Collectors.joining(" "));
   }
 }
